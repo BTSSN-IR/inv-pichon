38f8d1617cd4a3e9c378410b510cec266d411e98
Gwendal Troadec <gw8ndal@gmail.com>
2024-01-31 13:45:07 +0100

    db update

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
-
+from flask import Flask, request, url_for, redirect, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+
     return render_template('login.html')
