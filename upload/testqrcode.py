bc3b22a70fbf8c5a0b4a570184fbbea852236aba
Nolan Bailliet <nolan.bailliet@hotmail.com>
2024-02-12 14:17:06 +0100

    Gwendal

--- upload/testqrcode.py
+++ upload/testqrcode.py
@@ -7,16 +7,31 @@ def read_qr_code(filename):
 
 # Exemple d'utilisation
 url = read_qr_code("test_QRCode.png")
-print(url) 
-"""
+print(url) l
+
+
 import pyzbar.pyzbar
 import PIL.Image
 import webbrowser
 
 def read_qr_code(filename):
-    image = PIL.Image.open(filename)
+    image = PIL.Image.open('r',filename,encoding = 'utf-8')
     codes = pyzbar.pyzbar.decode(image)
-    return codes[0].data.decode() if codes else None
+    return codes[0].data.decode()
 
-url = read_qr_code("test_QRCode.png")
-webbrowser.open(url)
+url = read_qr_code('C:\Users\User\Documents\BTS\Projet E6.5\inv-pichon\inv-pichon\upload\QR_Code_example.png')
+print(url)
+
+"""
+
+import pyzbar.pyzbar
+from PIL import Image
+import webbrowser
+
+def read_qr_code(filename):
+    image = Image.open(filename)
+    codes = pyzbar.pyzbar.decode(image)
+    return codes[0].data.decode()
+
+url = read_qr_code('C:\Users\User\Documents\BTS\Projet E6.5\inv-pichon\inv-pichon\upload\QR_Code_example.png')
+print(url)
