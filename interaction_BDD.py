806ea4f4fd8bfd7e9a576439d369c258b2b0baa3
Gwendal Troadec <gw8ndal@gmail.com>
2024-06-25 10:16:15 +0200

    useless stuff moved to old-inv branch

--- interaction_BDD.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-
-conn = sqlite3.connect('inv_pichon.db')
-cur = conn.cursor()
-cur.execute("INSERT INTO Admins(username,password) VALUES ('tset','test')")
-conn.commit()
-conn.close()
