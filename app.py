9656bb2ee7d37dda45d395ce1957ae18dcad5277
Gwendal Troadec <gw8ndal@gmail.com>
2024-07-10 15:36:16 +0200

    qr codes border adjusted

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def logout():
 @app.route("/generate_qrcode/<filename>")
 def generate_qrcode(filename): # Utilisation de la biliothèque qrcode pour générer un QR Code avec les données demandées
     data = filename
-    qr = qrcode.QRCode(version = 1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size = 10, border = 4)
+    qr = qrcode.QRCode(version = 1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size = 10, border = 1)
     qr.add_data(data)
     qr.make(fit = True)
     image = qr.make_image(fill_color = "black", back_color = "white")
